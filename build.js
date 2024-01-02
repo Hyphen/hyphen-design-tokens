afcb6b4b40cf11f7aebb18dbfed02f85fcd57802
Matthew Osborn <matthew@osbornm.com>
2024-01-02 15:04:10 -0600

    feat: add our brand + local overrides

--- build.js
+++ build.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const fse = require('fs-extra');
 const StyleDictionary = require('style-dictionary');
-const getFigmaDocument = require('./utils/getFigmaDocument/getFigmaDocument');
-const parseFigmaDocumentTokens = require('./utils/parseFigmaDocumentTokens/parseFigmaDocumentTokens');
 const mapSemanticColors = require('./utils/mapSemanticColors/mapSemanticColors');
 const dictionaryConfig = require('./config.json');
 const utilityClass = require('./formats/utilityClass/utilityClass');
@@ -13,7 +11,7 @@ const customKebab = require('./transforms/customKebab/customKebab');
 const createIconComponents = require('./utils/createIconComponents/createIconComponents');
 
 console.log('Build started...');
-console.log('\n==============================================');
+console.log('==============================================');
 
 // Custom Filters
 StyleDictionary.registerFilter({
@@ -63,76 +61,67 @@ const FIGMA_TOKENS_DOCUMENT = '';
 const FIGMA_FILE_VERSION = '';
 
 
-// TODO: read a JSON file rather than getting a figma file
-// we could try JSON first then figma? or combine them for the future?
 
+try {
+  let properties = {};
 
-/**
- * Read tokens from FIGMA file.
- */
-getFigmaDocument(FIGMA_TOKENS_DOCUMENT, FIGMA_FILE_VERSION)
-  .then(response => response.json())
-  .then(figmaJson => {
-    /**
-     * Empty build directoty
-     */
-    fse.emptyDirSync('./build');
-    console.log('\nBuild directory cleared');
-    console.log('\n==============================================');
+  if (FIGMA_FILE_VERSION && FIGMA_TOKENS_DOCUMENT) {
+    console.log('Loading from Figma is not currently supported...');
+  }
 
-    /**
-     * Generate dictionary by recursively parsing FIGMA tokens document.
-     */
-    let properties = parseFigmaDocumentTokens(figmaJson.document);
+  if (fse.pathExistsSync('./localOverrides.json')) {
+    const localOverrides = require('./localOverrides.json');
+    properties = { ...properties, ...localOverrides };
+  }
 
-    /**
-     * Generate semantic (light, lighter, etc...) colors
-     * from lightness numbers (50, 100, etc...)
-     * It keeps the original colors as well as the semantic versions.
-     */
-    properties = mapSemanticColors(properties);
+  /**
+  * Generate semantic (light, lighter, etc...) colors
+  * from lightness numbers (50, 100, etc...)
+  * It keeps the original colors as well as the semantic versions.
+  */
+  properties = mapSemanticColors(properties);
 
-    /**
-     * Apply the configuration.
-     *
-     * IMPORTANT: the registration of custom transforms
-     * needs to be done BEFORE applying the configuration.
-     */
-    const StyleDictionaryExtended = StyleDictionary.extend({
-      properties,
-      platforms: dictionaryConfig.platforms,
-    });
-
-    // FINALLY, BUILD ALL THE PLATFORMS
-    StyleDictionaryExtended.cleanAllPlatforms();
-    StyleDictionaryExtended.buildAllPlatforms();
-    console.log('\n==============================================');
-    console.log('\nStyle dictionary build completed!');
-
-    // Process Icons
-    const iconsSource = './icons';
-    const iconsDestination = './build/icons/svg';
-    fse.copySync(iconsSource, iconsDestination);
-    console.log('\n==============================================');
-    console.log('\nIcons successfully copied to build');
-
-    // Create React components based on SVG icons.
-    createIconComponents();
-    console.log('\n==============================================');
-    console.log('\nReact icons created!');
-
-    // From the built dictionary, generate constants of all token options.
-    // File can't be required at the top since build files are a dependency for this function
-    // and they do not exist until the style dictionary is built.
-    const generateTokenTypes = require('./utils/generateTokenTypes/generateTokenTypes');
-    generateTokenTypes();
-    console.log('\n==============================================');
-    console.log('\nToken types generated!');
-
-    console.log('\n==============================================');
-    console.log('\nAll done!');
-    console.log('\n==============================================');
-  })
-  .catch(err => {
-    console.log(err);
+  /**
+   * Apply the configuration.
+   *
+   * IMPORTANT: the registration of custom transforms
+   * needs to be done BEFORE applying the configuration.
+   */
+  const StyleDictionaryExtended = StyleDictionary.extend({
+    properties,
+    platforms: dictionaryConfig.platforms,
   });
+
+  // FINALLY, BUILD ALL THE PLATFORMS
+  StyleDictionaryExtended.cleanAllPlatforms();
+  StyleDictionaryExtended.buildAllPlatforms();
+  console.log('\n==============================================');
+  console.log('\nStyle dictionary build completed!');
+
+  // Process Icons
+  const iconsSource = './icons';
+  const iconsDestination = './build/icons/svg';
+  fse.copySync(iconsSource, iconsDestination);
+  console.log('\n==============================================');
+  console.log('\nIcons successfully copied to build');
+
+  // Create React components based on SVG icons.
+  createIconComponents();
+  console.log('\n==============================================');
+  console.log('\nReact icons created!');
+
+  // From the built dictionary, generate constants of all token options.
+  // File can't be required at the top since build files are a dependency for this function
+  // and they do not exist until the style dictionary is built.
+  const generateTokenTypes = require('./utils/generateTokenTypes/generateTokenTypes');
+  generateTokenTypes();
+  console.log('\n==============================================');
+  console.log('\nToken types generated!');
+
+  console.log('\n==============================================');
+  console.log('\nAll done!');
+  console.log('\n==============================================');
+
+} catch (e) {
+  console.error(e);
+}
